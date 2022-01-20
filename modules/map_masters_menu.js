63ab603bc5f0f285f387a65d2e28774e06c61b7c
himmelweiss <himmelweiss22@hotmail.com>
2022-01-20 19:33:43 +0100

    add MapMenu to DSA GM Menu

--- modules/map_masters_menu.js
+++ modules/map_masters_menu.js
@@ -3,43 +3,22 @@ import MapMenu from "./map_menu.js";
 export default class MapMastersMenu {
     static registerButtons() {               
 
-        CONFIG.Canvas.layers.mapdsamenu = { layerClass: MapDSAMenuLayer, group: "primary" };
-
         game.dsa5.apps.mapMenu = new MapMenu();
 
         Hooks.on("getSceneControlButtons", btns => {
             if (game.user.isGM) {
-                const mapDSAMenuOptions = [{
+
+                const gmMenu = btns.find(x => x.name == "GM Menu")
+                gmMenu.tools.push(
+                    {
                         name: "MapMenu",
                         title: game.i18n.localize("mapGMMenu"),
                         icon: "fas fa-dsa5-map",
                         button: true,
                         onClick: () => { game.dsa5.apps.DSA5_Utility.renderToggle(game.dsa5.apps.mapMenu) }
                     }
-                ];
-                                
-                btns.push({
-                    name: "MapGMMenu",
-                    title: game.i18n.localize("mapGMMenu"),
-                    icon: "fas fa-dsa5-map",
-                    layer: "mapdsamenu",
-                    tools: mapDSAMenuOptions
-                });
+                )
             }
         });
     }
-
 }
-
-class MapDSAMenuLayer extends CanvasLayer {
-    static get layerOptions() {
-        return foundry.utils.mergeObject(super.layerOptions, {
-            name: "mapdsamenu",
-            canDragCreate: false,
-            controllableObjects: true,
-            rotatableObjects: true,
-            zIndex: 655,
-        });
-    }
-}
-
