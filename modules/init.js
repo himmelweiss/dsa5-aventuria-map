9656c625c8a298084adbb1afee357728cd2eeb53
himmelweiss <himmelweiss22@hotmail.com>
2022-02-25 09:11:44 +0100

    added english support

--- modules/init.js
+++ modules/init.js
@@ -19,12 +19,20 @@ Hooks.once("ready", function() {
         ChatMessage.create(game.dsa5.apps.DSA5_Utility.chatDataSetup(msg))
         game.settings.set("dsa5-aventuria-map", "firstTimeStart", true)
     }
-
-    game.dsa5.apps.journalBrowser.rshs.push({
-        id: "Interaktive Aventurienkarte",
-        path: "modules/dsa5-aventuria-map/adventurede.json",
-        visible: true
-    })
+	
+    if (game.i18n.lang == "de") {
+		game.dsa5.apps.journalBrowser.rshs.push({
+			id: "Interaktive Aventurienkarte",
+			path: "modules/dsa5-aventuria-map/adventurede.json",
+			visible: true
+		})
+    } else {
+		game.dsa5.apps.journalBrowser.rshs.push({
+			id: "Interactive Aventuria Map",
+			path: "modules/dsa5-aventuria-map/adventureen.json",
+			visible: true
+		})
+    }
 })
 
 Hooks.once('setup', () => {
